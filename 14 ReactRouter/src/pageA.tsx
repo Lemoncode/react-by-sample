fd329cee082744d3ee87d940b69dd46ae2d03ec1
Braulio <braulio.diez@gmail.com>
2016-10-25 12:33:17 +0100

    Basic React Router sample completed

---
+++ 14 ReactRouter/src/pageA.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import {Link} from 'react-router';
+
+export const PageA = () => {
+  return (
+    <div>
+      <h2> Hello from page A</h2>
+      <br/>
+      <Link to="/pageB">Navigate to Page B</Link>
+    </div>
+  )
+}
