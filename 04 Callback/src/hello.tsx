1a1512a25def58fee83a4b0c92d5948dd5c23b0e
David Atencia <david.atencia@gmail.com>
2016-10-29 21:23:56 +0200

    style(App): format the code

--- 04 Callback/src/hello.tsx
+++ 04 Callback/src/hello.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-export const HelloComponent = (props: {userName : string}) => {
+export const HelloComponent = (props: {userName: string}) => {
   return (
     <h2>Hello user: {props.userName} !</h2>
   );
-}
+};
