81ea12aea9bb1048a82a82f0ad64ce7a2ab6fdf4
Santiago Camargo Rodríguez <santi.camargo.rodriguez@gmail.com>
2016-10-30 23:00:33 +0100

    Review 03 State

--- 03 State/src/nameEdit.tsx
+++ 03 State/src/nameEdit.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 
-export const NameEditComponent = (props: {userName : string, onChange : (event : any) => any}) => {
+export const NameEditComponent = (props : { userName : string, onChange : (event : any) => any }) => {
   return (
     <div>
       <label>Update Name:</label>
-      <input value={props.userName} onChange={props.onChange}/>
+      <input value={props.userName} onChange={props.onChange} />
     </div>
   );
 }
