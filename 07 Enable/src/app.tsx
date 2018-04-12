57868cef61e3edea3d42a17e9a448784b7c7bac8
Braulio Diez Botella <brauliodiez@Braulios-MacBook-Pro.local>
2018-04-12 18:03:24 +0200

    07 enable ported

--- 07 Enable/src/app.tsx
+++ 07 Enable/src/app.tsx
@@ -1,44 +1,42 @@
 import * as React from 'react';
-import {HelloComponent} from './hello';
-import {NameEditComponent} from './nameEdit'
+import { HelloComponent } from './hello';
+import { NameEditComponent } from './nameEdit';
 
 interface Props {
-
 }
 
 interface State {
-  userName : string;
-  editingUserName : string;
+  userName: string;
+  editingUserName: string;
 }
 
 export class App extends React.Component<Props, State> {
-  constructor(props : Props) {
+  constructor(props: Props) {
     super(props);
 
     const defaultUserName = 'defaultUserName';
-    this.state = {userName: defaultUserName, editingUserName: defaultUserName};
+    this.state = { userName: defaultUserName, editingUserName: defaultUserName };
   }
 
   setUsernameState = () => {
-    this.setState({userName: this.state.editingUserName} as State);
+    this.setState({ userName: this.state.editingUserName } as State);
   }
 
-  updateEditingName = (editingName : string) : void => {
-    this.setState({editingUserName: editingName} as State);
+  updateEditingName = (editingName: string): void => {
+    this.setState({ editingUserName: editingName } as State);
   }
-      
+
 
   public render() {
     return (
-      <React.Fragment>
-        <HelloComponent userName={this.state.userName}/>
+      <>
+        <HelloComponent userName={this.state.userName} />
         <NameEditComponent
-            userName={this.state.userName}
-            editingUserName={this.state.editingUserName}
-            onEditingNameUpdated={this.updateEditingName}
-            onNameUpdateRequest={this.setUsernameState} />
-      </React.Fragment>
+          userName={this.state.userName}
+          editingUserName={this.state.editingUserName}
+          onEditingNameUpdated={this.updateEditingName}
+          onNameUpdateRequest={this.setUsernameState} />
+      </>
     );
   }
 }
-
--- 07 Enable/src/nameEdit.tsx
+++ 07 Enable/src/nameEdit.tsx
@@ -3,24 +3,20 @@ import {Fragment} from 'react';
 
 
 interface Props {
-    userName : string;
+  userName : string;
     editingUserName : string;
     onEditingNameUpdated : (newEditingName : string) => void;
     onNameUpdateRequest : () => void;  
 }
 
-  
 export const NameEditComponent = (props : Props) =>
-    <div>
-        <label>Update Name:</label>
-        <input value={props.editingUserName}
-          onChange={(e) : void => props.onEditingNameUpdated((e.target as HTMLInputElement).value)} />
+  <div>
+      <label>Update Name:</label>
+      <input value={props.editingUserName}
+        onChange={(e) : void => props.onEditingNameUpdated((e.target as HTMLInputElement).value)} />
 
-        <button 
-            className="btn btn-default" 
-            onClick={props.onNameUpdateRequest}
-            disabled={props.editingUserName === '' || props.userName === props.editingUserName}
-            >Change</button>
-    </div>
-
-
+      <button className="btn btn-default" 
+              onClick={props.onNameUpdateRequest}
+              disabled={props.editingUserName === '' || props.userName === props.editingUserName}
+              >Change</button>
+  </div>
