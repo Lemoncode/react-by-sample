e5b61369d911cedcd60e2bb177de81f34ae8ae5b
Sara Lissette Luis Ibáñez <lissette.ibnz@gmail.com>
2018-12-01 18:55:36 +0000

    Add files

---
+++ 12_TableHttp_Axios/src/memberHead.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { MemberEntity } from './model/member';
+
+export const MemberHead = () =>
+    <tr>
+        <th>
+            Avatar
+        </th>
+        <th>
+            Id
+        </th>
+        <th>
+            Name
+        </th>
+    </tr>
